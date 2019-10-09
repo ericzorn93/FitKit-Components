697ba75ae4ea41afb825dcd30f843d233c532972
Eric Zorn <ericzorndesigns@gmail.com>
2019-10-09 09:39:15 -0400

    Begin adding header component

---
+++ src/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./header.scss";
+
+interface Props {}
+
+const Header: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
